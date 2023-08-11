0bf436404dbb1559fa624338c7e2e89c7ae01f82
Travis <6611@holbertonstudents.com>
2023-08-11 11:23:11 -0700

    Realized I needed j and j +1 and not i

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -15,15 +15,15 @@ void bubble_sort(int *array, size_t size)
 
 	if (size < 2)
 		return;
-	for (j = 0; j < size - i; j++)
+	for (i = 0; i < size - 1; i++)
 	{
-		for (i = 0; i < size; i++)
+		for (j = 0; j < size - i - 1; j++)
 		{
-			if (array[i] > array[j])
+			if (array[j] > array[j + 1])
 			{
 				temp = array[j];
-				array[j] = array[i];
-				array[i] = temp;
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
 				print_array(array, size);
 			}
 		}
