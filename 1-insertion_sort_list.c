d7a726427c86a3b89ee4bdb09a2c9972eb59f03a
Travis <6611@holbertonstudents.com>
2023-08-11 13:12:29 -0700

    More variable names

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -28,7 +28,7 @@ void insertion_sort_list(listint_t **list)
 		else
 		{
 			earlier = after;
-			while (earlier->next != NULL && earlier->next->n <= now->data)
+			while (earlier->next != NULL && earlier->next->n <= now->n)
 				earlier = earlier->next;
 			now->next = earlier->next;
 			if (earlier->next != NULL)
