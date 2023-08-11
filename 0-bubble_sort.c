48a76b9a9f4c637035e337fecb3abce6963d5e5c
Travis <6611@holbertonstudents.com>
2023-08-11 09:28:38 -0700

    Initial push

---
+++ 0-bubble_sort.c
@@ -0,0 +1,31 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - Implements a bubble sort
+ * @array: The array to be sorted
+ * @size: The size of the array
+ *
+ * Return: No return 
+ */
+
+void bubble_sort(int *array, size_t size)
+{
+	char *temp = NULL;
+	int i = 0; j = 0;
+
+	if (size < 2)
+		return;
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+		{
+			if (array[i] > array[j])
+			{
+				temp = array[j];
+				array[j] = array[i];
+				array[i] = temp;
+				print_array(array);
+			}
+		}
+	}
+}
