4924765570f9fd56920c51cab4e5a6b93ecad5cc
Travis <6611@holbertonstudents.com>
2023-08-11 17:50:51 -0700

    Realized logic was wrong

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -15,19 +15,18 @@ void selection_sort(int *array, size_t size)
 
 	if (size < 2)
 		return;
-	for (i = 0; i < size; i++)
+	for (i = 0; i < size - 1; i++)
 	{
 		index = i;
-		for (j = 0; j < size - i - 1; j++)
+		for (j = i + 1; j < size; j++)
 		{
 			if (array[j] < array[index])
 				index = j;
 		}
-		temp = array[i];
-		array[i] = array[index];
-		array[index] = temp;
+		temp = array[index];
+		array[index] = array[i];
+		array[i] = temp;
 		if (index != i)
 			print_array(array, size);
-		i = index;
 	}
 }
